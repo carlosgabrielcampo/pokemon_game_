783d7763d8a24ac7245ef5202bef339bf15b8366
carlosgabrielcampo <carlosgabrielcampo@gmail.com>
2022-11-09 06:54:16 -0300

    created th initial folder

---
+++ pokemon.js
@@ -0,0 +1,21 @@
+const pikachu = {
+  hp: 100,
+  energia: 60,
+  level: 30,
+  defesa: 50,
+  ataques: { 
+    "Ataque Rápido": {
+      dano: 70,
+      energiaConsumida: 18,
+    },
+    "Investida Trovão": {
+      dano: 40,
+      energiaConsumida: 9,
+    },
+    "Choque do Trovão": {
+      dano: 90,
+      energiaConsumida: 28
+    }
+  } 
+}
+
